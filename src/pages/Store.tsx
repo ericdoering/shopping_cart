b448f2cc21dd364782a6f2ff3b9ffbc70a6c1786
Eric Doering <ericbenjamindoering@gmail.com>
2023-03-04 17:56:05 -0500

    addition of items json and initial setup of store component

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,16 @@
+import storeItems from "../data/items.json"
+import { Col, Row } from "react-bootstrap"
+
+
 export function Store() {
-    return <h1>Store</h1>
+    return (
+    <>
+        <h1>Store</h1>
+            <Row xs={1} md={2} lg={3} className="g-3">
+                {storeItems.map(item => (
+                    <Col>{JSON.stringify(item)}</Col>
+                ))}
+            </Row>
+    </>
+    )
 }
