1e3e8aea2f9ef7406f91f36cf50b6029767dc242
Eric Doering <ericbenjamindoering@gmail.com>
2023-03-15 08:13:10 -0400

    addition of ShoppingCartContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
 import { Navbar } from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 
 function App() {
   return (
-  <>
+  <ShoppingCartProvider>
     <Navbar />
       <Container className="mb-4">
         <Routes>
@@ -18,7 +19,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-  </>
+  </ShoppingCartProvider>
   )
 }
 
